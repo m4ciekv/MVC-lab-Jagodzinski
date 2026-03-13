086a2745a7b30975e7d613d4dfe5e8a65c7daef2
Maciej <maciekjagodzinski66@gmail.com>
2026-03-13 20:00:15 +0100

    Lab3 - Django tutorial completed

--- Django/polls/models.py
+++ Django/polls/models.py
@@ -1,5 +1,6 @@
 from django.db import models
-
+from django.utils import timezone
+import datetime
 
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
@@ -8,6 +9,8 @@ class Question(models.Model):
     def __str__(self):
         return self.question_text
 
+    def was_published_recently(self):
+        return timezone.now() - datetime.timedelta(days=1) <= self.pub_date <= timezone.now()
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
--- Django/polls/views.py
+++ Django/polls/views.py
@@ -1,14 +1,21 @@
-from django.http import HttpResponse
-from django.template import loader
 from django.shortcuts import render, get_object_or_404
-from .models import Question
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from .models import Choice, Question
 
+
+from django.utils import timezone
 
 def index(request):
-    latest_question_list = Question.objects.order_by("-pub_date")[:5]
-    template = loader.get_template("polls/index.html")
-    context = {"latest_question_list": latest_question_list}
-    return HttpResponse(template.render(context, request))
+    latest_question_list = Question.objects.filter(
+        pub_date__lte=timezone.now()
+    ).order_by("-pub_date")[:5]
+
+    return render(
+        request,
+        "polls/index.html",
+        {"latest_question_list": latest_question_list},
+    )
 
 
 def detail(request, question_id):
@@ -17,9 +24,15 @@ def detail(request, question_id):
 
 
 def results(request, question_id):
-    response = "You're looking at the results of question %s."
-    return HttpResponse(response % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, "polls/results.html", {"question": question})
 
 
 def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+
+    selected_choice = question.choice_set.get(pk=request.POST["choice"])
+    selected_choice.votes += 1
+    selected_choice.save()
+
+    return HttpResponseRedirect(reverse("polls:results", args=(question.id,)))
