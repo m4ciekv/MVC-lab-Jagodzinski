32417c708797f591dd7ff3bc5e60b460ebb05566
Maciej <maciekjagodzinski66@gmail.com>
2026-03-27 18:01:23 +0100

    Poprawiona wersja

--- Django/polls/views.py
+++ Django/polls/views.py
@@ -19,12 +19,12 @@ def index(request):
 
 
 def detail(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
+    question = get_object_or_404(Question, pk=question_id, pub_date__lte=timezone.now())
     return render(request, "polls/detail.html", {"question": question})
 
 
 def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
+    question = get_object_or_404(Question, pk=question_id, pub_date__lte=timezone.now())
     return render(request, "polls/results.html", {"question": question})
 
 
